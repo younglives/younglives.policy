2b9f3555c5559b2ecb6cf9a5807771faec820ecb
Michael Davis <m.r.davis@me.com>
2011-12-23 20:22:29 +0000

    Add a simple test

---
+++ younglives/policy/tests/base.py
@@ -0,0 +1,34 @@
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+
+from plone.testing import z2
+
+from younglives.policy.config import PROJECTNAME
+
+class TestCase(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import Products.OrderableReferenceField
+        self.loadZCML(package=Products.OrderableReferenceField)
+        import younglives.content
+        self.loadZCML(package=younglives.content)
+        import younglives.homepage
+        self.loadZCML(package=younglives.homepage)
+
+        # Install product and call its initialize() function
+        z2.installProduct(app, PROJECTNAME)
+
+    def setUpPloneSite(self, portal):
+        # Install into Plone site using portal_setup
+        self.applyProfile(portal, '%s:default' % PROJECTNAME)
+
+    def tearDownZope(self, app):
+        # Uninstall product
+        z2.uninstallProduct(app, PROJECTNAME)
+
+FIXTURE = TestCase()
+INTEGRATION_TESTING = IntegrationTesting(bases=(FIXTURE,), name="fixture:Integration")
