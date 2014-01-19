98d1170ef60b79879b6109cabbbc1bf35de52621
Michael Davis <m.r.davis@me.com>
2014-01-19 18:13:20 +0000

    Add a method to remove the gallery interface

---
+++ younglives/policy/setuphandlers.py
@@ -0,0 +1,24 @@
+import logging
+from zope.app.component.hooks import getSite
+from zope.interface import alsoProvides, noLongerProvides
+
+from StringIO import StringIO
+from Products.CMFCore.utils import getToolByName
+
+from younglives.content.interfaces.content import IGalleryContainerAware
+
+def importVarious(context):
+    """
+    Import various settings."""
+    # Only run step if a flag file is present
+    if context.readDataFile('younglives.policy.txt') is None:
+        return
+    site = context.getSite()
+    out = StringIO()
+
+    catalog = getToolByName(getSite(), 'portal_catalog')
+    items = catalog.searchResults({'object_provides':'IGalleryContainerAware.__identifier__',})
+    for item in items:
+        object = item.getObject()
+        noLongerProvides(object, IGalleryContainerAware)
+        object.reindexObject()
