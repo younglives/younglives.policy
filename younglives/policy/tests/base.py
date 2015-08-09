665ce5cbe4729314cbe53510a95f71b577a313b8
Michael Davis <m.r.davis@me.com>
2015-08-09 19:53:44 +0100

    Need to add all of the zcml

--- younglives/policy/tests/base.py
+++ younglives/policy/tests/base.py
@@ -13,12 +13,26 @@ class TestCase(PloneSandboxLayer):
 
     def setUpZope(self, app, configurationContext):
         # Load ZCML
+        import Products.Carousel
+        self.loadZCML(package=Products.Carousel)
+        import Products.CMFPlacefulWorkflow
+        self.loadZCML(package=Products.CMFPlacefulWorkflow)
         import Products.OrderableReferenceField
         self.loadZCML(package=Products.OrderableReferenceField)
+        import Products.RedirectionTool
+        self.loadZCML(package=Products.RedirectionTool)
+        import wildcard.media
+        self.loadZCML(package=wildcard.media)
+        import oxford.intranet.folder
+        self.loadZCML(package=oxford.intranet.folder)
         import younglives.content
         self.loadZCML(package=younglives.content)
         import younglives.homepage
         self.loadZCML(package=younglives.homepage)
+        import younglives.theme
+        self.loadZCML(package=younglives.theme)
+        import younglives.policy
+        self.loadZCML(package=younglives.policy)
 
         # Install product and call its initialize() function
         z2.installProduct(app, PROJECTNAME)
